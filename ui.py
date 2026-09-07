e55258bab242848e5e9b537dbf8a872d6174142b
Raimundothedev <pedrofioravantibachega@gmail.com>
2026-09-07 14:56:22 -0300

    Starting ui with ctk

--- ui.py
+++ ui.py
@@ -1,3 +1,10 @@
 import customtkinter as ctk
 
 ctk.set_appearance_mode("dark")
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title(f"FileOrganizer{version}")
+
+        
